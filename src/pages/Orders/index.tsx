64bcb412d15938370082d69c6323f300d6d39ca1
MaysharaF <mayshara@alunos.utfpr.edu.br>
2023-06-18 16:34:55 -0300

    feat: criando listagem de pedidos feitos

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React from "react";
 
-// import { Container } from './styles';
+import {Table} from 'antd'
+
+import { Container } from "./styles";
 
 const Orders: React.FC = () => {
-  return <div />;
-}
+
+  const dataSource = [
+    {
+      key: '1',
+      quantity: 1,
+      item: 'Orgulho e Preconceito',
+      total: "R$ 79,00"
+    },
+  
+  ];
+
+  const columns = [
+    {
+      title: '#',
+      dataIndex: 'key',
+      key: 'key',
+    },
+    {
+      title: 'Quantidade',
+      dataIndex: 'quantity',
+      key: 'quantity',
+    },
+    {
+      title: 'Item',
+      dataIndex: 'item',
+      key: 'item',
+    },
+    {
+      title: 'Total',
+      dataIndex: 'total',
+      key: 'total',
+    },
+  ];
+
+  
+
+  return (
+    <Container>
+      <h2>Seus pedidos</h2>
+      <Table dataSource={dataSource} columns={columns} />;
+    </Container>
+  );
+};
 
 export default Orders;
--- src/pages/Orders/styles.ts
+++ src/pages/Orders/styles.ts
@@ -1,5 +1,12 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+
+  h2 {
+    margin-bottom: 2%;
+  }
 `;

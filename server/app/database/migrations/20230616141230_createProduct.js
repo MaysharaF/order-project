22684933c2c8484795687bf6d83571655100c606
MaysharaF <mayshara@alunos.utfpr.edu.br>
2023-06-19 20:37:15 -0300

    feat: criando migration de produtos

---
+++ server/app/database/migrations/20230616141230_createProduct.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("products", function (table) {
+    table.increments("id");
+    table.string("title");
+    table.string("observation");
+    table.string("url_image");
+    table.decimal("price");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("products");
+};
